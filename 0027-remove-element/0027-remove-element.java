086e5825c21d2bec13a40205290b1bd32c6b099a
yashasviRGB <yashasvi.yadav@bounteous.com>
2026-03-30 16:49:42 +0530

    Time: 0 ms (100%), Space: 43.4 MB (86.13%) - LeetHub

--- 0027-remove-element/0027-remove-element.java
+++ 0027-remove-element/0027-remove-element.java
@@ -1,14 +1,14 @@
 class Solution {
     public int removeElement(int[] nums, int val) {
-        int k = 0; // position for next valid element
-        
+        int k = 0;
+
         for (int i = 0; i < nums.length; i++) {
             if (nums[i] != val) {
                 nums[k] = nums[i];
                 k++;
             }
         }
-        
-        return k;
+
+        return k;        
     }
 }
