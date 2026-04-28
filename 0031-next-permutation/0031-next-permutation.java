6e9ceaab06c87b1f87c48a0cd18d207ca25ef839
yashasviRGB <yashasvi.yadav@bounteous.com>
2026-04-28 11:37:17 +0530

    Time: 0 ms (100%), Space: 44.6 MB (51.13%) - LeetHub

---
+++ 0031-next-permutation/0031-next-permutation.java
@@ -0,0 +1,45 @@
+class Solution {
+    public void swap(int a , int b, int arr[]){
+        int temp = arr[a];
+        arr[a] = arr[b];
+        arr[b] = temp;
+    }
+
+    public void reverse(int i, int j , int[] arr){
+        while(i < j){
+            swap(i, j, arr);
+            i++;
+            j--;
+        }
+    }
+
+    public void nextPermutation(int[] nums) {
+        int n = nums.length;
+        int pivot = -1;
+
+        // Step 1: find pivot
+        for(int i = n - 2; i >= 0; i--){
+            if(nums[i] < nums[i+1]){
+                pivot = i;
+                break;
+            }
+        }
+
+        // Step 2: if no pivot → reverse whole array
+        if(pivot == -1){
+            reverse(0, n - 1, nums);
+            return;
+        }
+
+        // Step 3: find next greater element from right
+        for(int i = n - 1; i > pivot; i--){
+            if(nums[i] > nums[pivot]){
+                swap(i, pivot, nums);
+                break;
+            }
+        }
+
+        // Step 4: reverse suffix
+        reverse(pivot + 1, n - 1, nums);
+    }
+}
