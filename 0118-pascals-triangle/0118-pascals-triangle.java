c54f93b36cc69a605021441dce3d9c5adbf36c91
yashasviRGB <yashasvi.yadav@bounteous.com>
2026-04-28 10:37:24 +0530

    Time: 1 ms (95.52%), Space: 43.5 MB (34.66%) - LeetHub

---
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -0,0 +1,28 @@
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+        List<Integer> prev = new ArrayList<>();
+        List<Integer> curr = new ArrayList<>();
+        curr.add(1);
+        result.add(new ArrayList<>(curr));
+        if(numRows == 1) return result;
+        curr.add(1);
+        // curr.add(1);
+        result.add(new ArrayList<>(curr));
+        // prev = curr;
+        for(int i = 2; i < numRows; i++){
+            // prev.addAll(curr); // Wrong: prev keeps growing every iteration
+            prev = new ArrayList<>(curr);
+            curr = new ArrayList<>();        // fix: reset current row
+            curr.add(1);
+            for(int j = 1; j < i; j++){
+                int sum = prev.get(j) + prev.get(j-1);
+                curr.add(sum);
+                
+            }
+            curr.add(1);
+            result.add(new ArrayList<>(curr));
+        }
+        return result;
+    }
+}
