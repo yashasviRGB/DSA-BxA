6811ba5a9ec5af994e498b276e279261c737e2e5
yashasviRGB <yashasvi.yadav@bounteous.com>
2026-04-30 10:51:25 +0530

    Time: 0 ms (100%), Space: 42.2 MB (28.5%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.java
@@ -0,0 +1,39 @@
+class Solution {
+    int row;
+    int col;
+    int[][] dp;
+    public int solve(int i, int j){
+        
+        if(i >= row || j >= col){
+            return -1;
+        }
+        if(i == row - 1 && j == col - 1){
+            return 1;
+        }
+        if(dp[i][j] != 0) return dp[i][j];
+        int bottom = solve(i + 1, j);
+        int right = solve(i, j + 1);
+        if(bottom != -1 && right != -1){
+            dp[i][j] = bottom + right;
+        }
+        else if(bottom == -1 && right != -1){
+            dp[i][j] = right;
+        }
+        else if(bottom != -1 && right == -1){
+            dp[i][j] = bottom;
+        }
+        return dp[i][j];
+
+    }
+    public int uniquePaths(int m, int n) {
+        this.row = m;
+        this.col = n;
+        dp = new int[m][n];   // ✅ initialize here
+
+        // initialize dp with -1
+        // for(int i = 0; i < m; i++){
+        //     Arrays.fill(dp[i], -1);
+        // }
+        return solve(0,0);
+    }
+}
