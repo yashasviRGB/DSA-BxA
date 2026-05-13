29f359a3d328c66a3f55a02725badbccf88c43e6
yashasviRGB <yashasvi.yadav@bounteous.com>
2026-05-13 18:57:53 +0530

    Time: 1 ms (100%), Space: 43.6 MB (25.32%) - LeetHub

--- 2395-find-subarrays-with-equal-sum/2395-find-subarrays-with-equal-sum.java
+++ 2395-find-subarrays-with-equal-sum/2395-find-subarrays-with-equal-sum.java
@@ -1,12 +1,13 @@
 class Solution {
     public boolean findSubarrays(int[] nums) {
-        Map<Integer, Set<Integer>> mp = new HashMap<>();
+        Set<Integer> mp = new HashSet<>();
 
         int n = nums.length;
         for(int i = 0; i< n-1; i++){
             int sum = nums[i] + nums[i+1];
-            if(mp.containsKey(sum)) return true;
-            mp.computeIfAbsent(sum, k -> new HashSet<>()).add(i);
+            if(mp.contains(sum)) return true;
+            else mp.add(sum);
+            // mp.computeIfAbsent(sum, k -> new HashSet<>()).add(i);
         }
         // for(Set<Integer> s : mp.values()){
         //     if(s.size() > 1) return true;
