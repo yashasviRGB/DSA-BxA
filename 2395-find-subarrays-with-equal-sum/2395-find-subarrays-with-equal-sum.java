da63b0afb6a9c49c70fa2ef48da826616dcc91ac
yashasviRGB <yashasvi.yadav@bounteous.com>
2026-05-13 18:56:46 +0530

    Time: 2 ms (17.74%), Space: 43.8 MB (10%) - LeetHub

--- 2395-find-subarrays-with-equal-sum/2395-find-subarrays-with-equal-sum.java
+++ 2395-find-subarrays-with-equal-sum/2395-find-subarrays-with-equal-sum.java
@@ -5,11 +5,12 @@ class Solution {
         int n = nums.length;
         for(int i = 0; i< n-1; i++){
             int sum = nums[i] + nums[i+1];
+            if(mp.containsKey(sum)) return true;
             mp.computeIfAbsent(sum, k -> new HashSet<>()).add(i);
         }
-        for(Set<Integer> s : mp.values()){
-            if(s.size() > 1) return true;
-        }
+        // for(Set<Integer> s : mp.values()){
+        //     if(s.size() > 1) return true;
+        // }
         return false;
     }
 }
