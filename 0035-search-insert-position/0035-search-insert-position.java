cda8140f5dafa5d1bfd8d46c15487ce6a97026c0
yashasviRGB <yashasvi.yadav@bounteous.com>
2026-05-14 12:07:24 +0530

    Time: 0 ms (100%), Space: 44.9 MB (28.15%) - LeetHub

---
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int n = nums.length;
+        int low = 0;
+        int high =  n-1;
+        int mid = (low + high)/2;
+        while(low <= high){
+            if(nums[mid] == target) return mid;
+            else if(nums[mid] < target){
+                low = mid + 1;
+            }
+            else {
+                high = mid - 1;
+            }
+            mid = (low + high)/2;
+        }
+        return low;
+    }
+}
