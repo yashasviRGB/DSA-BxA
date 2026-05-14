6c6b8fff59888d02a652cab023e9e5c0898738ac
yashasviRGB <yashasvi.yadav@bounteous.com>
2026-05-14 19:25:31 +0530

    Time: 2 ms (87.77%), Space: 47.5 MB (94.81%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,14 +1,41 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int curr = nums[0];
         int n = nums.length;
-        if(n == 1) return true;
-        for(int i = 0; i < n; i++){
-            if(curr <= nums[i])
-                curr = nums[i];
-            if(curr == 0 && i != n-1) return false;
-            curr--;
+        int currJump = nums[0];
+        int i = 1;
+        for(; i< n-1; i++){
+            if(currJump <= 0) return false;
+            currJump -= 1;
+            
+            currJump = Math.max(currJump,nums[i]);
         }
+        if(i == n-1 && currJump <= 0) return false;
         return true;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+    // public boolean canJump(int[] nums) {
+    //     int curr = nums[0];
+    //     int n = nums.length;
+    //     if(n == 1) return true;
+    //     for(int i = 0; i < n; i++){
+    //         if(curr <= nums[i]){
+    //             curr = nums[i];
+    //         }
+    //         if(curr == 0 && i != n-1) return false;
+    //         curr--;
+    //     }
+    //     return true;
+    // }
