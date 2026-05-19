f312a7084a77926eea379f046571eaeb924e2fcd
yashasviRGB <yashasvi.yadav@bounteous.com>
2026-05-20 02:33:50 +0530

    Time: 26 ms (17.87%), Space: 47.8 MB (55.65%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.java
@@ -0,0 +1,30 @@
+class Solution {
+    public boolean check(int[] piles, int h, int value){
+        int totalhours = 0;
+        for(int num : piles){
+            totalhours += Math.ceil((double) num / value);
+        }
+        return totalhours <= h;
+    }
+    public int minEatingSpeed(int[] piles, int h) {
+        int n = piles.length;
+        int low = 1;
+        int high = Integer.MIN_VALUE;
+        for(int num : piles){
+            // low = Math.min(low,num);
+            high = Math.max(high,num);
+        }
+        int result = high;
+        while(low <= high){
+            int mid = low + (high - low)/2;
+            if(check(piles,h,mid)){
+                result = Math.min(result,mid);
+                high = mid - 1;
+            }
+            else{
+                low = mid + 1;
+            }
+        }
+        return result;
+    }
+}
