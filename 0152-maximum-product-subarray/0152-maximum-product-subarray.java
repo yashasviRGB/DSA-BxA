c0755e917bcb7203bc4b5ed79cff8d452b7707bb
yashasviRGB <yashasvi.yadav@bounteous.com>
2026-05-19 23:11:28 +0530

    Time: 0 ms (100%), Space: 47.5 MB (15.08%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.java
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.java
@@ -2,7 +2,7 @@ class Solution {
     public int maxProduct(int[] nums) {
         int n = nums.length;
         int max1 = Integer.MIN_VALUE;
-        int max2 = Integer.MIN_VALUE;
+        // int max2 = Integer.MIN_VALUE;
         int prod = 1;
         for(int i = 0; i<n; i++){
             prod *= nums[i];
@@ -12,9 +12,9 @@ class Solution {
         prod = 1;
         for(int i = n-1; i >= 0; i--){
             prod *= nums[i];
-            max2 = Math.max(max2, prod);
+            max1 = Math.max(max1, prod);
             if(prod == 0) prod = 1;
         }
-        return Math.max(max1, max2);
+        return max1;
     }
 }
