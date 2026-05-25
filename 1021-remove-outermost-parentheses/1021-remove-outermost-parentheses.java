6111079c497ede8d6abed8ce249809f9d2237f95
yashasviRGB <yashasvi.yadav@bounteous.com>
2026-05-25 17:05:03 +0530

    Time: 10 ms (13.06%), Space: 47.1 MB (5.82%) - LeetHub

--- 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
+++ 1021-remove-outermost-parentheses/1021-remove-outermost-parentheses.java
@@ -1,25 +1,57 @@
 class Solution {
     public String removeOuterParentheses(String s) {
-        Stack<Character> st = new Stack<>();
         int n = s.length();
         String result = "";
-        for(int i = 0; i<n; i++){
+        int open = 0;
+        for(int i = 0; i< n; i++){
             char ch = s.charAt(i);
-            if(ch == '('){
-                if(st.size() == 0){
-                    st.push(ch);
+            
+            if(open > 1){
+                result += ch;
+                if(ch == '(')
+                    open++;
+                else open--;
+            }
+            else if(open == 1){
+                if(ch == '('){
+                    result += ch;
+                    open++;
                 }
                 else{
-                    st.push(ch);
-                    result += ch;
+                    open--;
                 }
+                
             }
             else{
-                if(st.size() > 1)
-                    result += ch;
-                st.pop();
+                open++;
             }
+            
         }
         return result;
     }
+
+
+    // public solve(String s){
+    //     Stack<Character> st = new Stack<>();
+    //     int n = s.length();
+    //     String result = "";
+    //     for(int i = 0; i<n; i++){
+    //         char ch = s.charAt(i);
+    //         if(ch == '('){
+    //             if(st.size() == 0){
+    //                 st.push(ch);
+    //             }
+    //             else{
+    //                 st.push(ch);
+    //                 result += ch;
+    //             }
+    //         }
+    //         else{
+    //             if(st.size() > 1)
+    //                 result += ch;
+    //             st.pop();
+    //         }
+    //     }
+    //     return result;
+    // }
 }
