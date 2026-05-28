09406b5066b256da322507e48eea929bd87f29a9
yashasviRGB <yashasvi.yadav@bounteous.com>
2026-05-28 17:19:30 +0530

    Time: 1 ms (98.08%), Space: 42.1 MB (98.36%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -3,13 +3,17 @@ class Solution {
         if(x <= 1) return x;
         int low = 1;
         int high = x/2;
+        int result = -1;
         while(low <= high){
             int mid = low + (high - low)/2;
             long square = (long) mid * mid;
             if(square == x) return mid;
-            else if(square < x) low = mid + 1;
+            else if(square < x){
+                result = mid;
+                low = mid + 1;
+            }
             else high = mid - 1;
         }
-        return high;
+        return result;
     }
 }
