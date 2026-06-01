74d6d0d68c5a4d0dcc9e2525528efc212677c493
yashasviRGB <yashasvi.yadav@bounteous.com>
2026-06-01 10:41:57 +0530

    Time: 1 ms (95.23%), Space: 43.5 MB (32.45%) - LeetHub

--- 0118-pascals-triangle/0118-pascals-triangle.java
+++ 0118-pascals-triangle/0118-pascals-triangle.java
@@ -1,42 +1,58 @@
 class Solution {
+
     public List<List<Integer>> generate(int numRows) {
-
         List<List<Integer>> result = new ArrayList<>();
-        List<Integer> next = new ArrayList<>();
-
-        next.add(1);
+        if(numRows == 0) return result;
         
-        result.add(new ArrayList<>(next));
-        if(numRows == 1) return result;
-
-        next.add(1);
-
-        result.add(new ArrayList<>(next));
-        if(numRows == 2) return result;
-
-        List<Integer> prev;
-        for(int i = 2; i< numRows; i++){
-            // List<Integer> temp;
-            prev = new ArrayList<>(next);
-            next = new ArrayList<>();
-            next.add(1);
-            for(int j = 1; j < prev.size(); j++){
-                next.add(prev.get(j) + prev.get(j-1));
-            }
-            next.add(1);
-            
-            result.add(new ArrayList<>(next));
+        if(numRows == 1){
+            result.add(Arrays.asList(1));
+            return result;
         }
-        return result;
-
-
-
-
-
-
-
-
+        List<List<Integer>> prev = generate(numRows - 1);
+        List<Integer> currRow = new ArrayList<>();
+        currRow.add(1);
+        for(int i = 1; i < numRows- 1; i++){
+            currRow.add(prev.get(numRows - 2).get(i - 1) 
+                        + prev.get(numRows - 2).get(i));
+        }
+        currRow.add(1);
+        prev.add(currRow);
+        return prev;
     }
+
+    // public List<List<Integer>> generate(int numRows) {
+
+    //     List<List<Integer>> result = new ArrayList<>();
+    //     List<Integer> curr = new ArrayList<>();
+
+    //     curr.add(1);
+        
+    //     result.add(new ArrayList<>(curr));
+    //     if(numRows == 1) return result;
+
+    //     curr.add(1);
+
+    //     result.add(new ArrayList<>(curr));
+    //     if(numRows == 2) return result;
+
+    //     List<Integer> prev;
+    //     for(int i = 2; i< numRows; i++){
+    //         // List<Integer> temp;
+    //         prev = new ArrayList<>(curr);
+    //         curr = new ArrayList<>();
+    //         curr.add(1);
+    //         for(int j = 1; j < prev.size(); j++){
+    //             curr.add(prev.get(j) + prev.get(j-1));
+    //         }
+    //         curr.add(1);
+            
+    //         result.add(new ArrayList<>(curr));
+    //     }
+    //     return result;
+    // }
+
+
+
     // public List<List<Integer>> generate(int numRows) {
     //     List<List<Integer>> result = new ArrayList<>();
     //     List<Integer> prev = new ArrayList<>();
