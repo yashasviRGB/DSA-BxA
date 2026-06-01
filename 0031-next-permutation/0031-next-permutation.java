9d5f381ed13b78482873bf33dcceea7f537da27b
yashasviRGB <yashasvi.yadav@bounteous.com>
2026-06-02 01:39:57 +0530

    Time: 0 ms (100%), Space: 44.9 MB (14.59%) - LeetHub

--- 0031-next-permutation/0031-next-permutation.java
+++ 0031-next-permutation/0031-next-permutation.java
@@ -14,6 +14,37 @@ class Solution {
     }
 
     public void nextPermutation(int[] nums) {
+        int pivot = -1;
+        int n = nums.length;
+        for(int i = n-2; i>=0; i--){
+            if(nums[i] < nums[i+1]){
+                pivot = i;
+                break;
+            }
+        }
+        int swapping = Integer.MAX_VALUE;
+        int index = -1;
+        if(pivot == -1){
+            reverse(0,n-1,nums);
+            return;
+        }
+        for(int i = n - 1; i >= pivot + 1; i--){
+            if(nums[i] > nums[pivot] && nums[i] < swapping){
+                index = i;
+                swapping = nums[i];
+                swap(pivot,i,nums);
+                break;
+            }
+        }
+        reverse(pivot + 1,n-1,nums);
+
+    }
+
+
+
+
+    /*
+    public void nextPermutation(int[] nums) {
         int n = nums.length;
         int pivot = -1;
 
@@ -42,4 +73,8 @@ class Solution {
         // Step 4: reverse suffix
         reverse(pivot + 1, n - 1, nums);
     }
+    */
+
+
+
 }
