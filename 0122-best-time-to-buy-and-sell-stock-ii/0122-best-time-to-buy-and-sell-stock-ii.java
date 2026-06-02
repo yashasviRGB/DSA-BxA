243edcf5f0ab46cf81ef32ae23722327691a8065
yashasviRGB <yashasvi.yadav@bounteous.com>
2026-06-03 00:53:12 +0530

    Time: 0 ms (100%), Space: 46.6 MB (29.77%) - LeetHub

--- 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
+++ 0122-best-time-to-buy-and-sell-stock-ii/0122-best-time-to-buy-and-sell-stock-ii.java
@@ -4,11 +4,8 @@ class Solution {
         int prev = prices[0];
         int profit = 0;
         for(int i = 1; i< n; i++){
-            if(prices[i] >= prices[i-1]){
+            if(prices[i] > prices[i-1]){
                 profit += prices[i] - prices[i-1];
-            }
-            else{
-                continue;
             }
         }
         return profit;
