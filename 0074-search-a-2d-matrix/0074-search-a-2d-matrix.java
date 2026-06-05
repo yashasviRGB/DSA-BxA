f8f8008298c16d522ddafe12ce772f7843a70485
yashasviRGB <yashasvi.yadav@bounteous.com>
2026-06-05 10:42:24 +0530

    Time: 0 ms (100%), Space: 44.1 MB (8.12%) - LeetHub

--- 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
+++ 0074-search-a-2d-matrix/0074-search-a-2d-matrix.java
@@ -1,47 +1,22 @@
 class Solution {
-    public int findRow(int[][] matrix, int target){
-        int n = matrix.length;
-        int low = 0;
-        // int index = -1;
-        int high = n-1;
-        while(low <= high){
-            int mid = (low + high)/2;
-            if(matrix[mid][0] == target) return mid;
-            else if(matrix[mid][0] < target){
-                low = mid + 1;
-            }
-            else {
-                high = mid - 1;
-            }
-        }
-        return high;
-    }
-    public int findElement(int[][] matrix, int target, int index){
-        int n = matrix[0].length;
-        int low = 0;
-        // int result = -1;
-        int high = n-1;
-        while(low <= high){
-            int mid = (low + high)/2;
-            if(matrix[index][mid] == target){
-                // result = mid;
-                return mid;
-            }
-            else if(matrix[index][mid] < target){
-                low = mid + 1;
-            }
-            else {
-                high = mid - 1;
-            }
-        }
-        return -1;
-    }
     public boolean searchMatrix(int[][] matrix, int target) {
-        int index = findRow(matrix,target);
-        int result = -1;
-        if(index < 0) result = findElement(matrix,target,0);
-        else result = findElement(matrix,target,index);
-        
-        return result >= 0 ? true : false;
+        int n = matrix.length;
+        int m = matrix[0].length;
+        int row = 0;
+        int col = m-1;
+
+        while(row < n && col >= 0){
+            if(target < matrix[row][col]){
+                col--;
+            }
+            else if(target > matrix[row][col]){
+                row++;
+            }
+            else{
+                return true;
+            }
+        }
+        return false;
+
     }
 }
