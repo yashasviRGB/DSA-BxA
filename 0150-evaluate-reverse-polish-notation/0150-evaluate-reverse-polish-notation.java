31d8f49f31d2d57ae294e45bf41a0998e200f3f3
yashasviRGB <yashasvi.yadav@bounteous.com>
2026-08-25 16:35:28 +0530

    Time: 7 ms (35.36%), Space: 45.1 MB (89.86%) - LeetHub

---
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.java
@@ -0,0 +1,35 @@
+class Solution {
+    public int calculate(int a, int b, String ch){
+        switch(ch){
+            case "+": return a + b;
+            case "-": return a - b;
+            case "*": return a * b;
+            case "/": return a / b;
+        }
+        return 0;
+    }
+    public int precedence(String ch){
+        if(ch == "+" || ch == "-") return 1;
+        else if(ch == "*" || ch == "/") return 2;
+
+        return 0;
+    }
+    public int evalRPN(String[] tokens) {
+        Stack<Integer> st = new Stack<>();
+        int n = tokens.length;
+        for(int i = 0; i< n; i++){
+            String ch = tokens[i];
+            if(!ch.equals("+") && !ch.equals("-") && !ch.equals("*") && !ch.equals("/")){
+                st.push(Integer.parseInt(ch));
+            }
+            else{
+                int right = st.pop();
+                int left = st.pop();
+                int value = calculate(left,right,ch);
+                st.push(value);
+            }
+        }
+        if(!st.empty()) return st.pop();
+        return 0;
+    }
+}
